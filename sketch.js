578585febe6bb9d83a91f17f49f5a931aafbd2e6
Kheya-Mandal <76730767+Kheya-Mandal@users.noreply.github.com>
2021-01-29 11:29:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,13 +65,11 @@ function draw() {
             }
             if(b4.isTouching(bWhite)&&bWhite.bounceOff(b4)){
                 bWhite.shapeColor="yellow";
-                bWhite.velocityX=4;
+                bWhite.velocityX=-4;
                 bWhite.velocityY=-4;
                 music.play();
             }
-                  //if(bWhite.bounceOff(edges)){
                   
-                  //}
           
             drawSprites();
            
